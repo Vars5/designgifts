358a62eedadb2dce1781a340f70eeeeb926d467f
Pasha Rayan <pasha.rayan@gmail.com>
2012-07-19 14:35:13 +1000

    Got the pages to load and write to gift db

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -1,6 +1,6 @@
 class Gift < ActiveRecord::Base
   attr_accessible :gifter_id, :item, :message, :name, :owner_id
   
-  has_many :users
+  belongs_to :users
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,6 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name
   # attr_accessible :title, :body
 
-  belongs_to :gifts
+  has_many :gifts
 
 end
